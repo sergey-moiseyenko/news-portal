9e5985c403c1f922025390b529b295675e73aa86
Sergey Moiseyenko <smoiseyenko@gmail.com>
2017-04-09 15:23:31 +0300

    setup eslint

--- app-http.js
+++ app-http.js
@@ -1,35 +1,7 @@
 let http = require('http');
 let fs = require('fs');
 
-/*http.createServer((request, response) => {
-
-  if (request.url == '/') {
-    let file = new fs.ReadStream(__dirname + '/public/UI/html/index.html');
-    sendFile(file, response, {'Content-type': 'text/html'}) ;
-  }
-  else if(request.url.indexOf('.js') != -1){
-    let file = new fs.ReadStream(__dirname + '/public' + request.url);
-    sendFile(file, response, {'Content-type': 'text/javascript'});
-
-  }else if(request.url.indexOf('.css') != -1){
-    let file = new fs.ReadStream(__dirname + '/public' + request.url);
-    sendFile(file, response, {'Content-type': 'text/css'});
-  }
-  else if(request.url.indexOf('.jpg') != -1 || request.url.indexOf('.png') != -1) {
-    let file = new fs.ReadStream(__dirname + '/public' + request.url);
-    sendFile(file, response, {'Content-type': 'text/jpeg'});
-  }
-
-}).listen(8000);
-
-function sendFile(file, response, headers) {
-  response.writeHead(200, headers);
-  file.pipe(response);
-}
-
-*/
-
-var express = require('express'),
+let express = require('express'),
     app = express();
 
   app.use(express.static('public'));
